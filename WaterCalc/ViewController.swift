72f7edc5ece39ef6bc34871d799ef4785c6fb058
mzgk <mzgkworks@gmail.com>
2018-02-11 20:57:30 +0900

    Initial Commit

---
+++ WaterCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaterCalc
+//
+//  Created by 水垣岳志 on 2018/02/11.
+//  Copyright © 2018年 mzgkworks.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
